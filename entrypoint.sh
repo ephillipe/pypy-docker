cb5b37f24725c651bd573c36dd33ee2223ea8a03
Erick Almeida <erick.dnt@alterdata.com.br>
2015-10-28 09:04:09 -0200

    Update

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+APP_SCRIPT=${APP_SCRIPT:-application.py}
+APP_REQUIREMENTS=${APP_REQUIREMENTS:-requirements.txt}
+APP_PORT=${APP_PORT:-8080}
+
+echo "Application script: " $APP_SCRIPT
+echo "Application requirements: " $APP_REQUIREMENTS
+echo "Application port: " $APP_PORT
+
+echo "Preparing environment..."
+pip install --upgrade -r $APP_REQUIREMENTS
+
+echo "Starting application..."
+pypy $APP_SCRIPT --port=$APP_PORT
+
+exit 0
